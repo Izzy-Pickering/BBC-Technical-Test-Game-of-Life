f3aa01a375b1c3702f56ca198ca76c6bb727a7a8
Izzy-Pickering <71183187+Izzy-Pickering@users.noreply.github.com>
2021-07-01 22:35:04 +0200

    Update Game of life.py

--- Game of life.py
+++ Game of life.py
@@ -1,5 +1,6 @@
 # Import appropriate librarie
 import copy
+from sys import exit
 
 # LIVE and DEAD, two global constants to make it easy to change for testing (in case the values used are not the same as mine)
 LIVE = "l"
@@ -8,8 +9,7 @@ nextState = []
 
 def main():
     # Best to request user input instead of hard copying an initialState
-    # And then make sure the user respects the required criteria: 2D list with only "l" or "d" values (unless change the global constants)
-    # To make it easier to check the code directly, I have included an initial state in the code 
+    # However, to make it easier to check the code directly, I have included an initial state in the code 
     # This organism dies in 4 steps
     initialState = [
         [LIVE, DEAD, DEAD, DEAD, LIVE],
@@ -19,50 +19,67 @@ def main():
         [LIVE, DEAD, DEAD, LIVE, LIVE]
     ]
 
-    # Print the initial state
-    print("Initial state: ")
-    for i in range(len(initialState)):
-        print(initialState[i])
-    print("\nNext stage: ")
+    # Check the input is a 2D list
+    rightFormat = True
+    if initialState == []:
+        rightFormat = False
+    elif type(initialState) is list:
+        for value in initialState:
+            if type(value) is not list:
+                rightFormat = False
 
-    # Get the next stage
-    nextStateCreator(initialState)
-    
-    while True: 
-        # Check that the organism is not dead
-        organismIsDead = True
+    if rightFormat == True: 
+        # Print the initial state
+        print("Initial state: ")
+        for i in range(len(initialState)):
+            print(initialState[i])
 
-        for i in range(len(nextState)):
-            if nextState[i].count(LIVE) != 0:
-                organismIsDead = False
+        # Get the next stage
+        nextStateCreator(initialState)
+        
+        while True: 
+            # Check that the organism is not dead
+            organismIsDead = True
+            for i in range(len(nextState)):
+                if nextState[i].count(LIVE) != 0:
+                    organismIsDead = False
 
-        # If the organism is still alive, ask if the next stage is required
-        if organismIsDead == False:
-            response = input("\nWould you like to see the next state for this life form (y/n): ")
-            if response.lower().count("n") == 0:
-                newState = copy.deepcopy(nextState)
-                nextState.clear()
-                # Get the next stage
-                nextStateCreator(newState)
+            # If the organism is still alive, ask if the next stage is required
+            if organismIsDead == False:
+                response = input("\nWould you like to see the next state for this life form (y/n): ")
+                if response.lower().count("n") == 0:
+                    newState = copy.deepcopy(nextState)
+                    nextState.clear()
+                    # Get the next stage
+                    nextStateCreator(newState)
+                else:
+                    break
+                    
+            # If the organism is dead, quit the programme
             else:
-                break
-        # If the organism is dead
-        else:
-            print("This organim is dead.\n")
-            break 
+                print("This organim is dead.\n")
+                break 
+
+    else:
+        print("The input is not a 2D list")
 
 
 def nextStateCreator(initialState):
+    invalidInput = False
+
     for i in range(len(initialState)):
         newRow = []
-
         for j in range(len(initialState[i])):
+            # Check the cell is either live or dead. Exit the programme if there is an invalid value detected
+            if initialState[i][j] != DEAD and initialState[i][j] != LIVE:
+                exit("Invalid value inputted into the 2D list")
+            
             # Set a list of all the cells surrounding the current cell
             snapshot =[]
             rows = [i-1, i, i+1]
             columns = [j-1, j, j+1]
 
-            # Making sure the rows and columns are not out of index range
+            # Make sure the rows and columns are not out of index range
             for row in rows:
                 # Skip if the row is out of index range
                 if row < 0 or row >= len(initialState):
@@ -70,7 +87,7 @@ def nextStateCreator(initialState):
                 else:
                     for column in columns:
                         # Skip if the column is out of index range
-                        if column < 0 or column >= len(initialState[i]):
+                        if column < 0 or column >= len(initialState[row]):
                             pass
                         # Skip the current cell
                         elif row == i and column == j:
@@ -90,7 +107,12 @@ def nextStateCreator(initialState):
         # Add the row to the new state grid
         nextState.append(newRow)
 
-    # Print the new state
+    # Check if there was any changes. If not, quit the programme.
+    if initialState == nextState:
+        exit("\nThis organism is stable and will not change any further\n")
+
+    # Otherwise, print the new state
+    print("\nNext stage: ")
     for i in range(len(nextState)):
         print(nextState[i])
 
